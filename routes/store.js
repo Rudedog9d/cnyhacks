58af59ba6ff7997c84883640af596a117b78cddc
burrches <bryjeannotte@gmail.com>
2017-10-17 20:02:17 -0400

    Started layout of golden store

--- routes/store.js
+++ routes/store.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('store.html');
+    res.render('store/store.html');
+});
+
+/* GET home page. */
+router.get('/golden', function(req, res, next) {
+    res.render('store/golden/store.html');
 });
 
 module.exports = router;
