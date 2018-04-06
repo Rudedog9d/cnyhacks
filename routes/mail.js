05f439ccc97aba817316106b15895c9792d2c440
Brodie Davis <bdavis9d@gmail.com>
2018-04-05 23:24:54 -0400

    many updates to templating setup:  - add custom nunjucks filters to app.js  - add nunjucks util.html file with JSON encode/decode macros  - securely (I think) save user data to WebStore.User  - use live email data in mail list  - update some pages to use new nunjucks macros  - update user detail page to use riot tag arg rather than global var  - update mail api route to give mails based on current user

--- routes/mail.js
+++ routes/mail.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../core/db');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('mail/index.html');
+  res.redirect('/mail/inbox');
+});
+
+router.get('/:folder', function (req, res, next) {
+  let locals = {
+    folder: req.params.folder
+  };
+
+  db.getMail({
+    username: req.user.username,
+    folder: req.params.folder
+  }, function(err, data) {
+    if(err) {
+      return done(err);
+    }
+    locals.items = data;
+    res.render('mail/index.html', locals);
+  });
 });
 
 module.exports = router;
