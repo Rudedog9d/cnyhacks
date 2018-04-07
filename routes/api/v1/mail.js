bdced5b4067f0e667555d392cf1169e5760269f3
Brodie Davis <bdavis9d@gmail.com>
2018-04-06 20:40:23 -0400

    make DB query building functions public; add private DB _query for raw SQL queries; Add /api/v1/mail/folders route to return users' folders

--- routes/api/v1/mail.js
+++ routes/api/v1/mail.js
@@ -4,9 +4,26 @@ const apiUtils = require('../util');
 const db = require('../../../core/db');
 
 /**
+ * Get Current User's folders
+ */
+router.get('/folders.json', function (req, res, next) {
+  db.getFolders(req.user.username, function (err, data) {
+    if(err)
+      return apiUtils.apiError(res, err);
+
+    // Convert to array of folders
+    let ret = [];
+    for(obj of data)
+      ret.push(obj.folder);
+
+    apiUtils.apiDataResponse(res, ret);
+  })
+});
+
+/**
  * Get Current User's mail
  */
-router.get('/folder/:folder', function (req, res, next) {
+router.get('/folder/:folder.json', function (req, res, next) {
   db.getMail({
     username: req.user.username,
     folder: req.params.folder
